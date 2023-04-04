0428c5bda01dc263df13f0f2d3b5153b423debac
samiramrullah <samiramrullah@gmail.com>
2023-04-04 07:55:46 +0530

    sign up form added

--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -1,74 +1,128 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [acceptedTerms, setAcceptedTerms] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission
+    };
+
     return (
-        <> 
-            <div className='test'>
+        <>
             <Navbar />
-            </div>
-            <section class="h-100 gradient-form"  style={{backgroundColor:'#eee',marginTop:'7rem'}}>
-                <div class="container py-5 h-100">
-                    <div class="row d-flex justify-content-center align-items-center h-100">
-                        <div class="col-xl-10">
-                            <div class="card rounded-3 text-black">
-                                <div class="row g-0">
-                                    <div class="col-lg-6">
-                                        <div class="card-body p-md-5 mx-md-4">
-
-                                            <div class="text-center">
-                                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                                                     alt="logo" style={{width:'185px'}}/>
-                                                    <h4 class="mt-1 mb-5 pb-1">Alam Management</h4>
-                                            </div>
-
-                                            <form>
-                                                <p>Please login to your account</p>
-
-                                                <div class="form-outline mb-4">
-                                                    <input type="email" id="form2Example11" class="form-control"
-                                                        placeholder="Phone number or email address" />
-                                                    <label class="form-label" for="form2Example11">Username</label>
-                                                </div>
-
-                                                <div class="form-outline mb-4">
-                                                    <input type="password" id="form2Example22" class="form-control" />
-                                                    <label class="form-label" for="form2Example22">Password</label>
-                                                </div>
-
-                                                <div class="text-center pt-1 mb-5 pb-1">
-                                                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
-                                                        in</button>
-                                                    <a class="text-muted" href="#!">Forgot password?</a>
-                                                </div>
-
-                                                <div class="d-flex align-items-center justify-content-center pb-4">
-                                                    <p class="mb-0 me-2">Don't have an account?</p>
-                                                    <button type="button" class="btn btn-outline-danger">Create new</button>
-                                                </div>
-
-                                            </form>
-
+            <section className="bg-gray-50" style={{ marginTop: '8rem' }}>
+                <div className="container py-5">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-5">
+                            <div className="card shadow-sm border-0 rounded-lg">
+                                <div className="card-body p-4 p-sm-5">
+                                    <h1 className="h2 mb-4 fw-bold text-gray-900 text-center">
+                                        Create an account
+                                    </h1>
+                                    <form onSubmit={handleSubmit}>
+                                        <div className="mb-3">
+                                            <label
+                                                htmlFor="email"
+                                                className="form-label text-gray-900"
+                                            >
+                                                Your email
+                                            </label>
+                                            <input
+                                                type="email"
+                                                className="form-control rounded-lg"
+                                                id="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                placeholder="name@company.com"
+                                                required
+                                            />
                                         </div>
-                                    </div>
-                                    <div class="col-lg-6 d-flex align-items-center gradient-custom-2" style={{backgroundImage:'#5c636e'}}>
-                                        <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                            <h4 class="mb-4">We are more than just a company</h4>
-                                            <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                        <div className="mb-3">
+                                            <label
+                                                htmlFor="password"
+                                                className="form-label text-gray-900"
+                                            >
+                                                Password
+                                            </label>
+                                            <input
+                                                type="password"
+                                                className="form-control rounded-lg"
+                                                id="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                placeholder="••••••••"
+                                                required
+                                            />
                                         </div>
-                                    </div>
+                                        <div className="mb-3">
+                                            <label
+                                                htmlFor="confirm-password"
+                                                className="form-label text-gray-900"
+                                            >
+                                                Confirm password
+                                            </label>
+                                            <input
+                                                type="password"
+                                                className="form-control rounded-lg"
+                                                id="confirm-password"
+                                                value={confirmPassword}
+                                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                                placeholder="••••••••"
+                                                required
+                                            />
+                                        </div>
+                                        <div className="mb-3 form-check">
+                                            <input
+                                                type="checkbox"
+                                                className="form-check-input"
+                                                id="terms"
+                                                checked={acceptedTerms}
+                                                onChange={(e) => setAcceptedTerms(e.target.checked)}
+                                                required
+                                            />
+                                            <label
+                                                className="form-check-label text-gray-900"
+                                                htmlFor="terms"
+                                            >
+                                                I accept the{" "}
+                                                <a
+                                                    href="1"
+                                                    className="text-primary"
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    Terms and Conditions
+                                                </a>
+                                            </label>
+                                        </div>
+                                        <button
+                                            type="submit"
+                                            className="btn btn-primary btn-lg rounded-lg w-100"
+                                        >
+                                            Create an account
+                                        </button>
+                                        <p className="mt-3 text-center">
+                                            Already have an account?{" "}
+                                            <a href="1" className="text-primary">
+                                                Login here
+                                            </a>
+                                        </p>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <Footer/>
+            <Footer />
         </>
-    )
-}
+    );
+};
 
-export default Auth
+export default Auth;
